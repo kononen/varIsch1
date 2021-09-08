3732dd5d4c380e54c653f8332447c3247ebb98e3
ConstArch <90158169+ConstArch@users.noreply.github.com>
2021-09-08 15:06:22 +0300

    Update linsys.h

--- Прямые методы решения СЛАУ/linsys.h
+++ Прямые методы решения СЛАУ/linsys.h
@@ -1,8 +1,8 @@
-// Предварительный набросок
 #ifndef LINSYS
 #define LINSYS
 
 #include <cstring>
+#include <stdexcept>
 
 namespace linsys
 {
@@ -14,36 +14,30 @@ namespace linsys
 		using elem_type = __elem_type__;
 		
 	private:
-		elem_type **ptr;
+		elem_type *ptr;
 		
 	public:
-		matrix() : ptr = new elem_type*[dim]
+		matrix() : ptr = new elem_type[dim * dim] {}
+		
+		matrix(const matrix &other) : ptr = new elem_type[dim * dim]
 		{
-			for (std::size_t i = 0; i < dim; ++i)
-				ptr[i] = new elem_type[dim];
+			std::memcpy(ptr, other.ptr, sizeof(elem_type) * dim * dim);
 		}
 		
-		matrix(const matrix &other) : ptr = new elem_type*[dim]
-		{
-			for (std::size_t i = 0; i < dim; ++i)
-				std::memcpy(ptr[i], other.ptr[i], sizeof(elem_type) * dim);
-		}
-		
-		~matrix()
-		{
-			for (std::size_t i = 0; i < dim; ++i)
-				delete[] ptr[i];
-			delete[] ptr;
-		}
+		~matrix() { delete[] ptr; }
 		
 		elem_type &elem(const std::size_t i, const std::size_t j)
 		{
-			return ptr[i][j];
+			if (i >= dim || j >= dim)
+				throw std::out_of_range("Indexes must be non-negative values less than the dimension.");
+			return ptr[i * dim + j];
 		}
 		
 		const elem_type &elem(const std::size_t i, const std::size_t j) const
 		{
-			return ptr[i][j];
+			if (i >= dim || j >= dim)
+				throw std::out_of_range("Indexes must be non-negative values less than the dimension.");
+			return ptr[i * dim + j];
 		}
 	};
 	
@@ -67,13 +61,23 @@ namespace linsys
 		
 		~vector() { delete[] ptr; }
 		
-		elem_type &elem(const std::size_t i) { return ptr[i]; }
+		elem_type &elem(const std::size_t i)
+		{
+			if (i >= dim)
+				throw std::out_of_range("The index must be a non-negative value less than the dimension.");
+			return ptr[i];
+		}
 		
-		const elem_type &elem(const std::size_t i) const { return ptr[i]; }
+		const elem_type &elem(const std::size_t i) const
+		{
+			if (i >= dim)
+				throw std::out_of_range("The index must be a non-negative value less than the dimension.");
+			return ptr[i];
+		}
 	};
 	
 	template<std::size_t dim, class elem_type = double>
-	vector<dim, elem_type> solve(matrix<dim, elem_type> matr, vector<dim, elem_type> vect)
+	vector<dim, elem_type> Gaussian_method(matrix<dim, elem_type> matr, vector<dim, elem_type> vect)
 	{
 		for (std::size_t i = 0; i < dim; ++i)
 		{
@@ -83,6 +87,12 @@ namespace linsys
 			}
 		}
 		vector<dim, elem_type> result;
+	}
+	
+	template<std::size_t dim, class elem_type = double>
+	vector<dim, elem_type> QRdecompos_method(matrix<dim, elem_type> matr, vector<dim, elem_type> vect)
+	{
+		
 	}
 }
 
